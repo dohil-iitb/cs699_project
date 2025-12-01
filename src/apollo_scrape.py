209cdb52942267c694774233c46524a57f31fac6
dohil-iitb <25m0836@iitb.ac.in>
2025-12-02 01:19:34 +0530

    Delete src directory

--- src/apollo_scrape.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "https://www.apollopharmacy.in/otc/pacimol-650mg-tablet?doNotTrack=true"
-headers = {"User-Agent": "Mozilla/5.0"}
-
-response = requests.get(url, headers=headers)
-print(response.text)
--- src/medplus_scrape.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://www.medplusmart.com/drugsInfo/medicines/central-nervous-system_20004/anxiolytics_30035"
-headers = {"User-Agent": "Mozilla/5.0"}
-
-response = requests.get(url, headers=headers)
-print(response.text)
-
-
